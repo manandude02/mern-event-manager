ab0654c7e8279bffa50d767b6f9bddf298541ccd
Manan <manan.khandelwal@pw.live>
2025-10-30 22:26:11 +0530

    restart deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import store from './store'; // path to your store.js
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
